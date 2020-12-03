7a95515ac58a338c8a5af8bff40766845f6bb762
abiola farounbi <farounbiabiola@gmail.com>
2020-12-03 03:37:35 +0100

    added new files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,34 @@
 module.exports = {
-  purge: [],
+  purge: {
+    enabled: true,
+    content: [
+       './src/**/*.vue',
+      './public/**/*.html',
+    ]
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '500px'},
+      // => @media (max-width: 639px) { ... }
+    },
     extend: {},
   },
   variants: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
