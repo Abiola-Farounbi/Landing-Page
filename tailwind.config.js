294e40ea0ae8e662ac6fef2771af6a3ef927d15b
abiola farounbi <farounbiabiola@gmail.com>
2020-12-03 05:10:41 +0100

    added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   purge: {
-    enabled: true,
-    content: [
-       './src/**/*.vue',
-      './public/**/*.html',
-    ]
+
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
